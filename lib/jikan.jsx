8d0b5cd37ebe123985f45294a89ac2bd51fbfd5e
Douglas José Barboza <douglasbarbozajm@gmail.com>
2023-08-21 22:13:20 -0300

    delay api

--- lib/jikan.jsx
+++ lib/jikan.jsx
@@ -1,5 +1,5 @@
 function delay() {
-  return new Promise(resolve => setTimeout(resolve, 100))
+  return new Promise(resolve => setTimeout(resolve, 500))
 }
 
 export async function getSeasonNow(page) {
